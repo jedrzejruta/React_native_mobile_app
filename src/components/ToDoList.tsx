0cf91895b84a0deabee6754a2bdfde4f4e049386
jedrzejruta <48320979+jedrzejruta@users.noreply.github.com>
2020-04-22 00:58:32 +0200

    add scroll to top

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,27 +1,32 @@
-import React, { FC, useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { FC } from 'react';
+import { Text, Button } from 'react-native';
 import { useSelector } from 'react-redux';
+import { useScrollToTop } from '@react-navigation/native';
 
 import { IState } from '../reducers/index';
 import { IToDoListReducer  } from '../reducers/toDoListReducer';
 
 import { ISingleElList } from '../interfaces/ISingleElList';
 
+import * as Global from '../components/GlobalStyle';
+
 const ToDoList: FC<{switchView(formView: boolean)}> = props => {
-	const ToDoListState = useSelector<IState , IToDoListReducer>(state => state.ToDoList)
+	const ToDoListState = useSelector<IState , IToDoListReducer>(state => state.ToDoList);
 	const goToForm = () => {
 		props.switchView(true);
 	}
+	const ref = React.useRef(null);
+	useScrollToTop(ref);
 	return (
-		<View>
+		<Global.ListView ref={ref}>
 			{ToDoListState.ToDoList.map((el: ISingleElList, index: number) => 
-			<View key={index}>
-				<Text>{el.name}</Text>
-				<Text>{el.description}</Text>
-			</View>
+			<Global.SingleElListView key={index}>
+				<Text style={{padding: 5}}>{el.name}</Text>
+				<Text style={{padding: 5}}>{el.description}</Text>
+			</Global.SingleElListView>
 			)}
 			<Button title='Dodaj nowy' onPress={goToForm} />
-		</View>
+		</Global.ListView>
 	)
 };
 
