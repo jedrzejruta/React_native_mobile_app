d4fc1c9ec548bbdcf086b837b6ab6a164f378269
jedrzejruta <48320979+jedrzejruta@users.noreply.github.com>
2020-04-22 00:59:01 +0200

    add list style

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -2,10 +2,12 @@ import styled from 'styled-components/native';
 
 import Layout from '../constans/Layout';
 import Colors from '../constans/Colors';
+import { ScrollView } from 'react-native';
 
 
 //add flex
 export const CustomView = styled.View`
+	backgroundColor: red;
 	margin-top: ${Layout.statusBar}px;
 	height: ${Layout.window.height}px;
 `;
@@ -16,4 +18,19 @@ export const CustomText = styled.Text`
 	text-align: center;
 	color: ${Colors.black};
 	font-size: 20px;
+`;
+
+export const ListView = styled.ScrollView.attrs( () => ({
+	contentContainerStyle: {
+		alignItems: 'center',
+	},
+}))`
+	margin-top: ${Layout.statusBar}px;
+`;//bottom margin here
+
+export const SingleElListView = styled.View`
+	border: 1px solid black;
+	width: 100px;
+	margin: 0 0 10px 0;
+	padding: 2px;
 `;
