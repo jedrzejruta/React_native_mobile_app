3815215097d307ca100c6972792518bb55faa2c7
jedrzejruta <48320979+jedrzejruta@users.noreply.github.com>
2020-05-27 02:15:06 +0200

    add async assets loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import * as Contacts from 'expo-contacts';
 import { Gyroscope } from 'expo-sensors';
+
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
+import * as Fonts from 'expo-font';
+import { Entypo, MaterialIcons } from '@expo/vector-icons';
 
 import store from './src/tools/store';
 import Main from './src/Main';
@@ -12,7 +17,39 @@ export default function App() {
 		Contacts.getContactsAsync();
 	};
 
+	const [isReady, setReady] = useState(false);
+
+	const _cacheResourcesAsync = async () => {
+		const images = [
+			require('./src/assets/foto.png'),
+			require('./src/assets/foto2.png')
+		];
+
+		const icons = [
+			Entypo.font,
+			MaterialIcons.font
+		];
+
+		const cacheFonts = icons.map(icon => {
+			return Fonts.loadAsync(icon);
+		});
+
+		const cacheImages = images.map(image => {
+			return Asset.fromModule(image).downloadAsync();
+		});
+
+		await Promise.resolve(cacheImages);
+		await Promise.resolve(cacheFonts);
+	};
+
 	return (
+		isReady === false ? (
+			<AppLoading
+				startAsync={_cacheResourcesAsync}
+				onFinish={() => setReady(true)}
+				onError={console.warn}
+			/>
+				) :
 		<Provider store={store}>
 			<Main />
 		</Provider>
